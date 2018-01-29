696468b7b55366371e36d18a51b25dce6e10df5e
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2018-01-29 12:45:29 -0700

    Initial Commit

---
+++ STL-Vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STL-Vector
+//
+//  Created by Berrett, Calvin on 1/29/18.
+//  Copyright © 2018 Berrett, Calvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
