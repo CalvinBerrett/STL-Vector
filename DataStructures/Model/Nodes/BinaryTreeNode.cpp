16b7c2e9e890cc51b639e24b12866200fc83759b
CalvinBerrett <31518724+CalvinBerrett@users.noreply.github.com>
2018-03-27 12:50:57 -0600

    Added BinaryTreeNode class

---
+++ DataStructures/Model/Nodes/BinaryTreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  BinaryTreeNode.cpp
+//  DataStructures
+//
+//  Created by Berrett, Calvin on 3/27/18.
+//  Copyright © 2018 Berrett, Calvin. All rights reserved.
+//
+
+#include "BinaryTreeNode.hpp"
